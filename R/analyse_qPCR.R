c9e926836f9e8c2f9662b540a59df97b41ab9524
L-E-Bates <111574434+L-E-Bates@users.noreply.github.com>
2022-08-19 13:51:34 +0100

    Added required packages

--- R/analyse_qPCR.R
+++ R/analyse_qPCR.R
@@ -10,6 +10,7 @@
 #' @examples
 #' x <- "~/qPCR_results.csv"
 #' analyse_qPCR(x)
+#'
 analyse_qPCR <- function(qPCR_file){
   metadata <- read.csv(qPCR_file,header=FALSE, sep=",", encoding="UTF-8")
 
@@ -34,14 +35,14 @@ analyse_qPCR <- function(qPCR_file){
   data_small <- data[,c("Sample Name","Target Name","CT")]
   colnames(data_small) <- c("Sample_Name","Target_Name","CT")
   data_small$CT <- as.numeric(data_small$CT)
-  stat.summary <- na.omit(data_small) %>% group_by(Sample_Name, Target_Name) %>% summarise(mean=mean(CT), sd=sd(CT), lower_bound=mean(CT)-2*sd(CT), upper_bound=mean(CT)+2*sd(CT), outliers=CT<mean(CT)-2*sd(CT)|CT>mean(CT)+2*sd(CT))
+  stat.summary <- na.omit(data_small) %>% dplyr::group_by(Sample_Name, Target_Name) %>% dplyr::summarise(mean=mean(CT), sd=sd(CT), lower_bound=mean(CT)-2*sd(CT), upper_bound=mean(CT)+2*sd(CT), outliers=CT<mean(CT)-2*sd(CT)|CT>mean(CT)+2*sd(CT))
   data_small$CT[is.na(data_small$CT)] <- 40
   data_small$dCT <- data_small$CT-stat.summary$mean[match(paste(data_small$Sample_Name, ctrl_gene), paste(stat.summary$Sample_Name, stat.summary$Target_Name))]
   data_small$Exp_NegdCT <- 2^(-data_small$dCT)
-  dCT.summary <- na.omit(data_small) %>% group_by(Sample_Name, Target_Name) %>% summarise(mean=mean(dCT))
+  dCT.summary <- na.omit(data_small) %>% dplyr::group_by(Sample_Name, Target_Name) %>% dplyr::summarise(mean=mean(dCT))
   data_small$ddCT <- data_small$dCT-dCT.summary$mean[match(paste(ctrl_line,data_small$Target_Name), paste(dCT.summary$Sample_Name, dCT.summary$Target_Name))]
   data_small$Exp_NegddCT <- 2^(-data_small$ddCT)
-  data.summary <- na.omit(data_small) %>% group_by(Sample_Name, Target_Name) %>% summarise(mean_d=mean(Exp_NegdCT), sd_d=sd(Exp_NegdCT), mean_dd=mean(Exp_NegddCT), sd_dd=sd(Exp_NegddCT))
+  data.summary <- na.omit(data_small) %>% dplyr::group_by(Sample_Name, Target_Name) %>% dplyr::summarise(mean_d=mean(Exp_NegdCT), sd_d=sd(Exp_NegdCT), mean_dd=mean(Exp_NegddCT), sd_dd=sd(Exp_NegddCT))
   data.summary
 }
 
